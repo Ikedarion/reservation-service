0f06b8161a31300c93139859c78b974c97732337
ikeda rion <ike0324rion@icloud.com>
2024-12-15 11:39:10 +0900

    アップデート

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Faker\Factory as Faker;
+use Carbon\Carbon;
 
 
 use function Ramsey\Uuid\v1;
@@ -20,13 +21,31 @@ class UserSeeder extends Seeder
     {
         $faker = Faker::create('ja_JP');
 
-        $startingIndex = 23;
-        foreach (range($startingIndex, $startingIndex + 15) as $index) {
+        $startingIndex = 3;
+
+        User::create([
+            'name' => $faker->name,
+            'email' => 'user@example.com',
+            'password' => bcrypt('Password1'),
+            'role' => 'ユーザー',
+            'email_verified_at' => Carbon::now(),
+        ]);
+
+        User::create([
+            'name' => $faker->name,
+            'email' => 'admin@example.com',
+            'password' => bcrypt('Password1'),
+            'role' => '管理者',
+            'email_verified_at' => Carbon::now(),
+        ]);
+
+        foreach (range($startingIndex, $startingIndex + 19) as $index) {
             User::create([
                 'name' => $faker->name,
                 'email' => 'test' . $index . '@example.com',
                 'password' => bcrypt('Password1'),
-                'role' => 'ユーザー',
+                'role' => '店舗代表者',
+                'email_verified_at' => Carbon::now(),
             ]);
         }
     }
